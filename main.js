f0991ff10bbaf35cbe520614b23a074746cd713d
tehaon <kenny@pwnlog.com>
2022-05-21 04:19:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-import { Observable, Subject, ReplaySubject, from, of, range } from 'https://dev.jspm.io/rxjs@6/_esm2015';
-import { map, filter, switchMap, interval } from 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/8.0.0-alpha.3/rxjs.umd.min.js';
+//import { Observable, Subject, ReplaySubject, from, of, range } from 'https://dev.jspm.io/rxjs@6/_esm2015';
+//import { map, filter, switchMap } from 'https://dev.jspm.io/rxjs@6/_esm2015/operators';
 
 
-let interval$ = interval(10000)
+if (typof rxjs == 'undefined') {
+  var script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/6.1.0/rxjs.umd.js';
+  script.setAttribute('crossorigin',"anonymous");
+  document.head.appendChild(script);
+}
+
+console.log('rxjs installed', rxjs);
+
+let interval$ = rxjs.interval(10000)
 
 interval$.subscribe(val => console.log('val', val));
