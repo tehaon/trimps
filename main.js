a7cac3fe00007bc268d85be643087e8279297017
tehaon <kenny@pwnlog.com>
2022-05-21 04:30:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 //import { Observable, Subject, ReplaySubject, from, of, range } from 'https://dev.jspm.io/rxjs@6/_esm2015';
 //import { map, filter, switchMap } from 'https://dev.jspm.io/rxjs@6/_esm2015/operators';
+'https://unpkg.com/rxjs@7.5.5/dist/bundles/rxjs.umd.min.js'
 
+// if (typeof rxjs == 'undefined') {
+//   var script = document.createElement('script');
+//   script.src = 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/6.1.0/rxjs.umd.js';
+//   script.setAttribute('crossorigin',"anonymous");
+//   document.head.appendChild(script);
+// }
 
-if (typeof rxjs == 'undefined') {
-  var script = document.createElement('script');
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/6.1.0/rxjs.umd.js';
-  script.setAttribute('crossorigin',"anonymous");
-  document.head.appendChild(script);
-}
+// console.log('rxjs installed', rxjs);
 
-console.log('rxjs installed', rxjs);
+import rxjs from 'https://dev.jspm.io/rxjs@6';
+const { Observable } = rxjs;
+import operators from 'https://dev.jspm.io/rxjs@6/operators';
+const { map, interval } = operators;
 
-let interval$ = rxjs.interval(10000)
+let interval$ = interval(10000)
 
 interval$.subscribe(val => console.log('val', val));
