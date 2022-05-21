81fec83747bca7b9137ce35565625bc071a4ec5e
tehaon <kenny@pwnlog.com>
2022-05-21 01:49:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { Observable, Subject, ReplaySubject, from, of, range } from 'https://dev.jspm.io/rxjs@6/_esm2015';
-import { map, filter, switchMap, interval } from 'https://dev.jspm.io/rxjs@6/_esm2015/operators';
+import { map, filter, switchMap, interval } from 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/8.0.0-alpha.3/rxjs.umd.min.js';
 
 
 let interval$ = interval(10000)
